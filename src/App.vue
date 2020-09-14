99dde3f3209b06235296871b9ac5318a55b3e9a2
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-09-14 14:23:01 -0500

    Header

--- src/App.vue
+++ src/App.vue
@@ -36,18 +36,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background-color: #15202b;
 }
 </style>
--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,11 +1,71 @@
 <template>
   <div>
+    <b-navbar toggleable="lg" type="dark" class="NavBar" >
+      <b-navbar-brand :to="{name: 'Home'}">
+        <img
+          src= "@/assets/imgs/logo.svg"
+          alt=""
+          width="100px"
+          height="70px"
+        >
+      </b-navbar-brand>
 
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav v-if="user">
+        <b-nav-item-dropdown text="Historias"  right>
+            <b-dropdown-item @click="$router.push({name: 'Historias'})">Crear nueva</b-dropdown-item>
+            <b-dropdown-item @click="$router.push({name: 'Consultas'})">Consultar</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right v-if="user">
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <em>{{user.displayName || user.email}}</em>
+              </template>
+              <!-- TODO: change url-->
+              <b-dropdown-item :to="{name: 'verRegistros'}" disabled>Profile</b-dropdown-item>
+              <b-dropdown-item href="#" @click="logOut">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-navbar-brand class="brand" v-else @click="$router.push({name: 'Login'})">Sign In</b-navbar-brand>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex'
+import { auth } from 'firebase'
+import { signOut } from '../../backend/controllers/AuthController'
+
 export default {
-  name: 'Header'
+  name: 'Header',
+  computed: {
+    ...mapGetters('auth', ['user'])
+  },
+  methods: {
+    ...mapMutations('auth', ['SET_USER']),
+    logOut () {
+      signOut()
+      this.$router.push({ name: 'Login' })
+    }
+  },
+  created () {
+    // eslint-disable-next-line
+    auth().onAuthStateChanged(u => this.SET_USER(u))
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .NavBar {
+    background: transparent;
+    .brand {
+      cursor: pointer;
+    }
+  }
+</style>
